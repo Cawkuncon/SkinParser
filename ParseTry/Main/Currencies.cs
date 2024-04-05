93788cb4560b27f4b671dd94ff30ff874e8b9f41
Cawkuncon <homenet10@yandex.ru>
2024-04-05 16:51:21 +0300

    получение актуального курса юаня

---
+++ ParseTry/Main/Currencies.cs
@@ -0,0 +1,13 @@
+﻿namespace ParseTry.Main
+{
+	public class Currencies
+	{
+		public string ID { get; set; }
+		public int NumCode { get; set; }
+		public string CharCode { get; set; }
+		public int Nominal { get; set; }
+		public string Name { get; set; }
+		public decimal Value { get; set; }
+		public decimal Previous { get; set; }
+	}
+}
--- ParseTry/Main/ValueСurrency.cs
+++ ParseTry/Main/ValueСurrency.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +8,15 @@ using System.Threading.Tasks;
 
 namespace ParseTry.Main
 {
-    public static class ValueСurrency
-    {
-        public static decimal cnyValue = 12.7501M;
-    }
+	public static class ValueСurrency
+	{
+		public static decimal cnyValue = GetCnyRubValue() ?? 12.7501M;
+		private static decimal? GetCnyRubValue()
+		{
+			var data = new HttpClient().GetStringAsync("https://www.cbr-xml-daily.ru/daily_json.js").Result;
+			JObject.Parse(data).TryGetValue("Valute", out JToken valutes);
+			IEnumerable<Currencies> listCurrency = valutes.Select(val => JsonConvert.DeserializeObject<Currencies>(val.First.ToString()));
+			return listCurrency.Where(val => val.CharCode == "CNY").FirstOrDefault().Value;
+		}
+	}
 }
