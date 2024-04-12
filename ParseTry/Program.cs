1a848c6c7862d6d24dce4f416b421ea3c5b5e69b
Cawkuncon <homenet10@yandex.ru>
2024-04-12 10:52:45 +0300

    Консольный ввод цен и сессии, консольный вывод об этапе работы

--- ParseTry/Program.cs
+++ ParseTry/Program.cs
@@ -13,17 +13,29 @@ namespace ParseTry
         static void Main(string[] args)
         {
             CultureInfo.CurrentCulture = CultureInfo.GetCultureInfo("en-US");
+
+            Console.WriteLine("Начало работы парсера по маркету");
             using (var marketParser = new ParserMarket())
             {
                 marketParser.Parse();
             }
+			Console.WriteLine("Конец работы парсера по маркету");
 
-            CollectorClass.AggressiveCollectAllGen();
+			CollectorClass.AggressiveCollectAllGen();
 
-            var buffprsr = new ParserBuff(10, 1000, "session="); //минимальная цена поиска предметов в Юанях, максимальная цена в Юанях, код сессии
+            Console.WriteLine("Введите код сессии");
+            var session = Console.ReadLine();
+            Console.WriteLine("Введите минимальную цену в юанях");
+            var minPrice = decimal.Parse(Console.ReadLine());
+			Console.WriteLine("Введите максимальную цену в юанях");
+            var maxPrice = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("Начало работы парсера по баффу");
+            var buffprsr = new ParserBuff(minPrice, maxPrice, $"session={session}"); //минимальная цена поиска предметов в Юанях, максимальная цена в Юанях, код сессии
             buffprsr.TryParseInitialization();
+			Console.WriteLine("Конец работы парсера по баффу");
 
-            TransformClass.TransformToResult();
+			TransformClass.TransformToResult();
         }
     }
 }
